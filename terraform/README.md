872a28aa5172a2f841f1c90598b0e9bbb6c0dbc9
fadilmuh22 <fadilmuh22@gmail.com>
2023-06-30 17:53:58 +0700

    feat: ssl and load balancer, init service for node, php, and go

---
+++ terraform/README.md
@@ -0,0 +1,3 @@
+# Learn Terraform on DigitalOcean
+
+This is a companion repository to the [DigitalOcean Terraform provider tutorial](https://developer.hashicorp.com/terraform/tutorials/applications/digitalocean-provider).
---
+++ terraform/main.tf
@@ -0,0 +1,40 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MIT
+
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "digitalocean" {}
+
+resource "digitalocean_droplet" "orchestra-drop" {
+  count     = 3
+  image     = "ubuntu-22-04-x64"
+  name      = "orchestra-drop-${count.index}"
+  region    = "sgp1"
+  size      = "s-1vcpu-1gb"
+  ssh_keys  = [data.digitalocean_ssh_key.keys.id]
+}
+
+resource "digitalocean_project_resources" "project" {
+  project = data.digitalocean_project.orchestra.id
+  resources = flatten([
+    digitalocean_droplet.orchestra-drop[*].urn
+  ])
+}
+
+
+data "digitalocean_ssh_key" "keys" {
+  name = "fadillads-ubuntu"
+}
+
+data "digitalocean_project" "orchestra" {
+  name = "orchestra"
+}
